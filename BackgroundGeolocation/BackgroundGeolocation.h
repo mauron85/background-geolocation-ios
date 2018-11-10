657b8707934f89ab32fb87d1b4888b78cd7fc95d
Marian Hello <m.hello@gmail.com>
2018-11-10 19:01:57 +0100

    install CocoaLumberjack from Carthage

---
+++ BackgroundGeolocation/BackgroundGeolocation.h
@@ -0,0 +1,23 @@
+//
+//  BackgroundGeolocation.h
+//  BackgroundGeolocation
+//
+//  Created by Marian Hello on 10/11/2018.
+//  Copyright © 2018 mauron85. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BackgroundGeolocation.
+FOUNDATION_EXPORT double BackgroundGeolocationVersionNumber;
+
+//! Project version string for BackgroundGeolocation.
+FOUNDATION_EXPORT const unsigned char BackgroundGeolocationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BackgroundGeolocation/PublicHeader.h>
+
+
+#import <BackgroundGeolocation/MAURBackgroundGeolocationFacade.h>
+#import <BackgroundGeolocation/MAURConfig.h>
+#import <BackgroundGeolocation/MAURBackgroundTaskManager.h>
+#import <BackgroundGeolocation/MAURProviderDelegate.h>
